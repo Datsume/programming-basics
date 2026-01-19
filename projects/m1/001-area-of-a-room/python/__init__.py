5bb630410ee91376741356ffb8684142becd9d97
Datsume <zuzzilla@gmail.com>
2026-01-19 23:11:41 +0100

    Area

---
+++ projects/m1/001-area-of-a-room/python/__init__.py
@@ -0,0 +1,6 @@
+Lunghezza = float(input ("Inserire la lunghezza: "))
+Larghezza = float(input ("Inserire la larghezza: "))
+print ("Lunghezza = " , Lunghezza, "m")
+print ("Larghezza = " , Larghezza, "m")
+Area= Lunghezza * Larghezza
+print ("Area:", Area, "mq" )
